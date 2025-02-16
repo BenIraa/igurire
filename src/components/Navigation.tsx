885801b57b9ba8f7d9ea915b4e5737458b9ef7f1
Ben Iraa <63826242+BenIraa@users.noreply.github.com>
2025-02-16 09:28:00 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -43,7 +43,7 @@ export const Navigation = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0 font-semibold text-xl">
-            SMM Panel
+            iGurire
           </div>
 
           {/* Desktop Navigation */}
